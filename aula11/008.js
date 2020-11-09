09fc79e6c4131660fcbd8b92e482de88435e54a3
Joaoflavo <joaoflavo2@hotmail.com>
2020-11-09 16:48:57 -0300

    21

---
+++ aula11/008.js
@@ -0,0 +1,7 @@
+var vel = 60.5.toFixed(2)
+
+console.log(`A velocidade de seu carro é ${vel} km/h `)
+if (vel>60) {// condição simples
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') {
+    console.log('Voce é Brasiliero!')
+} else {
+    console.log(' Você é Estrangeiro')
+}
