383ae65c9ae789632491548e88f21cc4daeb999b
Joaoflavo <joaoflavo2@hotmail.com>
2020-11-11 21:19:38 -0300

    30

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -3,7 +3,7 @@ console.log (`Você tem ${age} anos, seu voto e igual à:` )
 if (age < 16){
     console.log('Não Vota!')
 } else if (age < 18 || age > 65){     
-        console.log ('Voto Opcional!')
-    } else if (age >= 18){
+    console.log ('Voto Opcional!')
+} else if (age >= 18){
     console.log('Voto Obrigatório!')
 }
