a42459bf21231245c139008fc7f2a6ff889b2711
Joaoflavo <joaoflavo2@hotmail.com>
2020-11-14 10:28:48 -0300

    32

--- aula14/Js/acess.js
+++ aula14/Js/acess.js
@@ -1,7 +1,7 @@
 function carregar(){
-    var msg = window.document.getElementById('msg')
-    var txt = window.document.getElementById('txt')
-    var img = window.document.getElementById('imagem')
+    var msg = window.document.getElementById('msg')//msg 1 da div html
+    var txt = window.document.getElementById('txt')//msg 2 da div html
+    var img = window.document.getElementById('imagem')//aponta para img do html
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
@@ -9,18 +9,18 @@ function carregar(){
     msg.innerHTML = `Agora são exatamente ${hora}:${minutos}:${second.toFixed()} horas.`
     if (hora >= 0 && hora < 12){
         // carrega bom dia
-        img.src = 'img/manhaedit.png'
+        img.src = 'img/manhaedit.png'//busca img para exibir em tela
         document.body.style.background = 'trasparent'
-        txt.innerHTML = 'Bom dia!'
+        txt.innerHTML = 'Bom dia!'//escreve em tela
     } else if (hora >= 12 && hora <= 18){
         //carrega boa tarde
-        img.src = 'img/tardeedit.png'
+        img.src = 'img/tardeedit.png'//busca img para exibir em tela
         document.body.style.background = 'trasparent'
-        txt.innerHTML = 'Boa tarde!'
+        txt.innerHTML = 'Boa tarde!'//escreve em tela
     } else {
         //carrega boa noite
-        img.src = 'img/noiteedit.png'
+        img.src = 'img/noiteedit.png'//busca img para exibir em tela
         document.body.style.background = 'trasparent'//Se usa # e o codigo hexadecimal da cor ex: #515154
-        txt.innerHTML = 'Boa Noite!'
+        txt.innerHTML = 'Boa Noite!'//escreve em tela
     } 
 }
